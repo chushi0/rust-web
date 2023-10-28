d8d7edac0494c253aa6e1a18e9721a930be9ded8
chushi0 <chushi0@yeah.net>
2023-10-28 16:02:48 +0800

    err report

--- web-bss/src/main.rs
+++ web-bss/src/main.rs
@@ -1,12 +1,14 @@
+use std::collections::HashMap;
+
 use crate::model::Model;
-use rocket::serde::json::Json;
+use rocket::{serde::json::Json, Request};
 
 #[macro_use]
 extern crate rocket;
 
 mod biz;
-pub(crate) mod model;
-pub(crate) mod service;
+pub mod model;
+pub mod service;
 
 #[get("/home/events")]
 async fn home_events() -> Json<Model<biz::home::GetEventsResp>> {
@@ -36,6 +38,28 @@ async fn mc_globaldata_advancement() -> Json<Model<biz::mc::GetAdvancementResp>>
     }))
 }
 
+#[catch(500)]
+async fn generate_error(req: &Request<'_>) {
+    let warn_msg = {
+        let method = req.method();
+        let path = req.uri().path();
+        let uri = req.uri();
+
+        format!("[ Method ] {method}\n[ Path ] {path}\n [ Uri ] {uri}")
+    };
+
+    let notification_params = {
+        let mut map = HashMap::new();
+        map.insert("warn_title".to_string(), "HTTP接口500报警".to_string());
+        map.insert("warn_msg".to_string(), warn_msg);
+        map
+    };
+    let result = feishu_api::sdk::send_card_message("ctp_AAwmLGzY0vLz", notification_params).await;
+    if let Err(err) = result {
+        log::error!("send notification error! err={err}")
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     let routes = routes![
@@ -43,5 +67,7 @@ fn rocket() -> _ {
         mc_playerdata_advancement,
         mc_globaldata_advancement
     ];
-    rocket::build().mount("/api/", routes)
+    rocket::build()
+        .register("/", catchers![generate_error])
+        .mount("/api/", routes)
 }
