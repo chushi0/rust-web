0965d4a33f4e7b73581a1c4c3e48d95a7ae0ac03
chushi0 <chushi0@yeah.net>
2023-10-29 19:45:26 +0800

    make code compile-able

--- web-bss/src/handler_grpc.rs
+++ web-bss/src/handler_grpc.rs
@@ -4,7 +4,6 @@ use volo_grpc::{Request, Response, Status};
 
 pub struct S;
 
-#[volo::async_trait]
 impl idl_gen::bss_websocket::BssWebsocketService for S {
     async fn send_room_common_change(
         &self,
