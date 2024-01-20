03c22015e900f0ef86e7eca649c63f43aeaa03c1
chushi0 <chushi0@yeah.net>
2024-01-21 00:33:01 +0800

    apply clippy

--- server-lib/feishu-api/src/api/mod.rs
+++ server-lib/feishu-api/src/api/mod.rs
@@ -2,4 +2,4 @@ pub mod auth;
 pub mod message;
 
 pub static USER_ID: &str = env!("RUST_WEB_FEISHU_USER_ID");
-pub(self) const FEISHU_HOST: &str = "https://open.feishu.cn";
+const FEISHU_HOST: &str = "https://open.feishu.cn";
