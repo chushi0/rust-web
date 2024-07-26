8576c09a4b70c5cab69abca3b3ba17f85704beaf
初始状态0 <chushi0@users.noreply.github.com>
2024-07-27 06:39:25 +0800

    use runtime env

--- server-lib/feishu-api/src/api/mod.rs
+++ server-lib/feishu-api/src/api/mod.rs
@@ -1,5 +1,11 @@
+use std::sync::OnceLock;
+
 pub mod auth;
 pub mod message;
 
-pub static USER_ID: &str = env!("RUST_WEB_FEISHU_USER_ID");
+static USER_ID: OnceLock<&'static str> = OnceLock::new();
 const FEISHU_HOST: &str = "https://open.feishu.cn";
+
+pub fn get_user_id() -> &'static str {
+    USER_ID.get_or_init(|| std::env::var("RUST_WEB_FEISHU_USER_ID").unwrap().leak())
+}
